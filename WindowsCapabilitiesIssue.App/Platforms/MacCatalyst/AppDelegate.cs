b583906eabc47fe3548a569afcbde44858de2cd5
Jonas Marvin Huhndorf <jonas.huhndorf@gmail.com>
2022-09-23 14:57:38 +0200

    init

---
+++ WindowsCapabilitiesIssue.App/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace WindowsCapabilitiesIssue.App;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
